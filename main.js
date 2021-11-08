11947177d10278d264a1319b93517cf79be6092c
Eric Filishtiner <eric.filishtiner@gmail.com>
2021-11-08 08:59:30 -0800

    Hashed no-container

---
+++ main.js
@@ -0,0 +1,11 @@
+const gitalk = new Gitalk({
+    clientID: '1c9bf295e604aea0e7cf',
+    clientSecret: 'ee7dfeeef186985585ab324c3b86275c78cd83ef',
+    repo: 'gitalk-comments',      // The repository of store comments,
+    owner: 'eric-filishtiner',
+    admin: ['eric-filishtiner'],
+    id: location.pathname,      // Ensure uniqueness and length less than 50
+    distractionFreeMode: false  // Facebook-like distraction free mode
+  })
+  
+  gitalk.render('gitalk-container')
